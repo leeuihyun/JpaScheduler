9aec7682ad686a3558973507e9cd9ff63db66020
leeuihyun <meta212121@naver.com>
2025-05-20 18:17:39 +0900

    feat : User domain 설정 User 관련 DTO, ENTITY 설정

---
+++ src/main/java/com/scheduler/hyun/domain/dto/user/UserUpdateRequest.java
@@ -0,0 +1,24 @@
+package com.scheduler.hyun.domain.dto.user;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class UserUpdateRequest {
+
+    @NotNull
+    private Long userId;
+
+    @NotBlank
+    @Size(min = 2, max = 10)
+    private String userName;
+
+    @Email
+    @NotBlank
+    private String userEmail;
+}
