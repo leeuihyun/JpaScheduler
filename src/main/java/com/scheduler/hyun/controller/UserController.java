897601b9a0103f96b10fd93398fcdcf1355a3063
leeuihyun <meta212121@naver.com>
2025-05-26 13:17:33 +0900

    refactor : @Controller -> RestController 로 수정

--- src/main/java/com/scheduler/hyun/controller/UserController.java
+++ src/main/java/com/scheduler/hyun/controller/UserController.java
@@ -10,8 +10,8 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotNull;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -20,18 +20,15 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
+@RequiredArgsConstructor
 @Validated
 @RequestMapping("/api/user")
 public class UserController {
 
     private final UserService userService;
-
-    public UserController(UserService userService) {
-
-        this.userService = userService;
-    }
 
     @PostMapping("/create")
     public ResponseEntity<UserIdResponse> createUser(
